20c5a37909b041828c153b23fd05c1032d7d3090
Leigh Williamson <leighw@us.ibm.com>
2020-12-13 17:53:54 -0600

    fix bug in assistant config

--- public/assistant_config.js
+++ public/assistant_config.js
@@ -1,6 +1,6 @@
 var assistant_config = {
     "page_title":"CapMetro Virtual Assistant",
-    "integrationID":"fd5ef423-e50b-403e-8094-04b2e51e56cc"
+    "integrationID":"fd5ef423-e50b-403e-8094-04b2e51e56cc",
     "region": "us-south",
     "bot_titlebar":"CapMetro Virtual Assistant"
 }
