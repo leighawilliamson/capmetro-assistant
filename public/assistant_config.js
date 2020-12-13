a8492e2d7ff6a53a8402273ea77af3988a60f649
Leigh Williamson <leighw@us.ibm.com>
2020-12-13 14:53:33 -0600

    initial code drop

---
+++ public/assistant_config.js
@@ -0,0 +1,7 @@
+var assistant_config = {
+    "page_title":"CapMetro Virtual Assistant",
+    "integrationID":"fd5ef423-e50b-403e-8094-04b2e51e56cc"
+    "region": "us-south",
+    "bot_titlebar":"CapMetro Virtual Assistant"
+}
+export default assistant_config;
